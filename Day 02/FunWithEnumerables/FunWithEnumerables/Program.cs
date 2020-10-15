30d0091d7f83c1ad147e064f972062ed3378fb32
Kobi Hari <kobi2294@yahoo.com>
2020-10-15 19:43:41 +0300

    Deffered enumerables C# 3

--- Day 02/FunWithEnumerables/FunWithEnumerables/Program.cs
+++ Day 02/FunWithEnumerables/FunWithEnumerables/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,23 +10,68 @@ namespace FunWithEnumerables
 {
     class Program
     {
+        private static Random rand = new Random();
+
+
         static void Main(string[] args)
         {
-            var fibonacci = new FibonacciSequence();
+            var sequence = GetSomeRandomNumbers(10).ToList();
 
 
-            var enumerator = fibonacci.GetEnumerator();
 
-            while (enumerator.MoveNext())
+            //var enumerator = fibonacci.GetEnumerator();
+
+            //while (enumerator.MoveNext())
+            //{
+            //    Console.WriteLine(enumerator.Current);
+            //}
+
+            //enumerator.Dispose();
+
+            foreach (var current in sequence)
             {
-                Console.WriteLine(enumerator.Current);
+                Console.WriteLine(current);
             }
 
-            enumerator.Dispose();
+            Console.ForegroundColor= ConsoleColor.Red;
+
+            foreach (var current in sequence)
+            {
+                Console.WriteLine(current);
+            }
 
 
             Console.ReadLine();
 
+        }
+
+        public static IEnumerable<int> GetFibonacci(int max)
+        {
+            int i = 1;
+            int j = 1;
+            int current = 1;
+
+            yield return 1;
+            yield return 1;
+
+            while (current < max)
+            {
+                var temp = current;
+                current = temp + j;
+                i = j;
+                j = temp;
+
+                yield return current;
+            }
+
+        }
+
+        public static IEnumerable<int> GetSomeRandomNumbers(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return rand.Next(100);
+            }
         }
     }
 }
