fe149686a47b944afa228b3d952b3f489d2cf361
Kobi Hari <kobi2294@yahoo.com>
2020-10-20 17:51:32 +0300

    LINQ - Part 1

---
+++ Day 03/FunWithLinq/FunWithLinq/NumberOperators.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithLinq
+{
+    public static class NumberOperators
+    {
+        //public static IEnumerable<int> MultiplyBy10(this IEnumerable<int> source)
+        //{
+        //    foreach (var num in source)
+        //    {
+        //        yield return num * 10;
+        //    }
+        //}
+
+        //public static IEnumerable<int> Add15(this IEnumerable<int> source)
+        //{
+        //    foreach (var num in source)
+        //    {
+        //        yield return num + 15;
+        //    }
+        //}
+
+        public static IEnumerable<K> Map<T, K>(this IEnumerable<T> source, Func<T, K> projection)
+        {
+            foreach (var num in source)
+            {
+                yield return projection(num);
+            }
+        }
+
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Predicate<T> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (predicate(item)) 
+                    yield return item;
+            }
+        }
+
+        public static IEnumerable<int> Digits(this int i)
+        {
+            var res = i.ToString()
+                .Select(c => (int)c - (int)'0');
+
+            return res;
+        }
+
+
+    }
+}
---
+++ Day 03/FunWithLinq/FunWithLinq/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BasicOperatorsExample();
+            Console.ReadLine();
+        }
+
+        #region How LINQ is constructed
+
+        public static void UsingOurOwnOperatorsExample()
+        {
+            var myNumbers = GenerateSomeNumbers()
+                .Map(n => n * 10)
+                .Map(n => n + 15)
+                .Filter(n => n % 3 == 0)
+                .Map(n => n.ToString());
+
+            var myStrings = GenerateSomeStrings()
+                .Filter(s => s.Length > 4)
+                .Map(s => s.ToLower())
+                .Map(s => string.Join("", s.Reverse()));
+
+            foreach (var num in myNumbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            foreach (var str in myStrings)
+            {
+                Console.WriteLine(str);
+            }
+        }
+
+        public static void QuerySyntaxExample()
+        {
+
+            var query1 = GenerateSomeNumbers()
+                .Where(n => n > 40)
+                .Select(n => n.ToString());
+
+            var query2 = from n in GenerateSomeNumbers()
+                         where n > 40
+                         select n.ToString();
+
+        }
+
+        #endregion
+
+        #region LINQ Operators
+
+        public static void BasicOperatorsExample()
+        {
+            // Select
+            // Where
+            // OrderBy 
+
+            var numbers = GenerateSomeNumbers();
+            numbers = numbers.OrderBy(n => n);
+
+            var amount = numbers.Count();
+
+            var first = numbers.Last();
+            var list = numbers.ToList();
+            var array = numbers.ToArray();
+            var hashset = numbers.ToHashSet();
+            var numbersByString = numbers.ToDictionary(num => num.ToString());
+
+            var numbersBySumOfDigits = numbers.ToLookup(num => num.Digits().Sum());
+
+
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            //Console.WriteLine(string.Join(", ", numbers));
+
+            numbers = GenerateSomeNumbers()
+                .OrderBy(n => n.Digits().Sum())
+                .ThenBy(n => n % 10);
+
+            Console.WriteLine(string.Join(", ", numbers));
+
+        }
+
+        #endregion
+
+        public static IEnumerable<int> Inifinte()
+        {
+            var i = 0;
+            while (true)
+            {
+                yield return i;
+                i += 1;
+            }
+        }
+
+        public static IEnumerable<int> GenerateSomeNumbers()
+        {
+            yield return 123;  // 6
+            yield return 154;  // 10
+            yield return 142; // 7
+            yield return 233; // 8
+            yield return 25;  // 7
+            yield return 35;  // 8
+            yield return 262; // 10
+            yield return 100; // 1
+            yield break;
+        }
+
+        public static IEnumerable<string> GenerateSomeStrings()
+        {
+            yield return "Hello";
+            yield return "World";
+            yield return "How";
+            yield return "Are";
+            yield return "You";
+
+        }
+    }
+}
