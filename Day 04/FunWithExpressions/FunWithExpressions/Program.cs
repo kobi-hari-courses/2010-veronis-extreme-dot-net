7daf94b1084c686efb365c8968b1a8c8cdc2873b
Kobi Hari <kobi2294@yahoo.com>
2020-10-22 19:54:08 +0300

    intro to expression

---
+++ Day 04/FunWithExpressions/FunWithExpressions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> func = i => i * (2 + 3) - 20;
+
+            Expression<Func<int, int>> exp = i => i * (2 + 3) - 20;
+
+
+            Console.WriteLine(func);
+            Console.WriteLine(exp);
+
+            var res = func.Invoke(10);
+
+            var res2 = exp.Compile().Invoke(10);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
