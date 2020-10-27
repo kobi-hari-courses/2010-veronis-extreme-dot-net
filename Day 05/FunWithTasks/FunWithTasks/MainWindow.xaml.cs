74f09884e0fa6f41a0653f0a2b5e09076988f775
Kobi Hari <kobi2294@yahoo.com>
2020-10-27 18:51:52 +0200

    TPL - Part 1

---
+++ Day 05/FunWithTasks/FunWithTasks/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FunWithTasks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string a = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            var g = new Grid();
+        }
+
+        private async void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("1");   // now
+
+            var count = await _run();
+
+            Debug.WriteLine("2");   // later
+        }
+
+        private async Task<int> _run()
+        {
+            Task<List<int>> task = null;
+
+            try
+            {
+                Debug.WriteLine("A");   // now
+                progressBar.IsIndeterminate = true;
+                txtStatus.Text = "Please Wait";
+                btnGo.IsEnabled = false;
+                btnCancel.IsEnabled = true;
+
+                task = PrimesCalculator.GetAllPrimesAsync(0, 250000);
+
+                Debug.WriteLine("B");   // now
+                var results = await task;
+                Debug.WriteLine("C");   // later
+
+                lstResults.ItemsSource = results;
+                txtStatus.Text = "Done";
+
+                Debug.WriteLine("D");// later
+
+                return results.Count;
+            }
+            catch (Exception ex)
+            {
+                txtStatus.Foreground = Brushes.Red;
+                txtStatus.Text = ex.Message;
+            }
+            finally
+            {
+                progressBar.IsIndeterminate = false;
+                btnGo.IsEnabled = true;
+                btnCancel.IsEnabled = false;
+            }
+
+            return 0;
+        }
+
+
+
+        private void btnGo_Click_old(object sender, RoutedEventArgs e)
+        {
+            progressBar.IsIndeterminate = true;
+            txtStatus.Text = "Please Wait";
+            btnGo.IsEnabled = false;
+            btnCancel.IsEnabled = true;
+            var task = PrimesCalculator.GetAllPrimesAsync(0, 250000);
+
+            var tcont = task.ContinueWith(_ =>
+            {
+                try
+                {
+                    var results = task.Result;
+
+                    lstResults.ItemsSource = results;
+                    txtStatus.Text = "Done";
+                    progressBar.IsIndeterminate = false;
+                    btnGo.IsEnabled = true;
+                    btnCancel.IsEnabled = false;
+
+                }
+                catch (AggregateException ex)
+                {
+
+
+                }
+
+                catch (InvalidOperationException ex)
+                {
+
+                }
+                return "Hello";
+
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+
+        }
+    }
+}
---
+++ Day 05/FunWithTasks/FunWithTasks/PrimesCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithTasks
+{
+    public static class PrimesCalculator
+    {
+        private static bool _isPrime(int number)
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+
+        private static IEnumerable<int> _getAllPrimes(int start, int finish)
+        {
+            for (int i = start; i <= finish; i++)
+            {
+                //if (i == 100000)
+                //    throw new InvalidOperationException("Becuase i feel like it, ha ha ha");
+
+                if (_isPrime(i)) yield return i;
+            }
+        }
+
+        public static List<int> GetAllPrimes(int start, int finish)
+        {
+            return _getAllPrimes(start, finish).ToList();
+        }
+
+        public static Task<List<int>> GetAllPrimesAsync(int start, int finish)
+        {
+            return Task.Factory.StartNew(() => GetAllPrimes(start, finish));
+        }
+    }
+}
